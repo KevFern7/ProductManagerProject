e2c0ff4352a1bd1adc6f2ebc605a85f9e2fc6261
KevFern7 <kevin21177@gmail.com>
2026-02-09 17:38:05 -0500

    Added discount feature by Kevin Fernandes

--- src/ProductManager/ProductManager.java
+++ src/ProductManager/ProductManager.java
@@ -4,5 +4,10 @@ public class ProductManager {
 
     public double calculatePriceAfterTax(double price, double taxRate) {
         return price + (price * taxRate);
+        
     }
-}
+        public double calculateDiscount(double price, double discountRate) {
+            return price - (price * discountRate);
+        }
+
+    }
